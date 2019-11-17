f25d7db48879b48243cd67089bfef3ae6da4a536
“joaotps22” <joaovictortps22@gmail.com>
2019-11-17 20:38:43 -0300

    Crindo banco de dados (notas_online) e sua conexao

---
+++ conexao.php
@@ -0,0 +1,9 @@
+<?php
+$servername = "localhost"; //padrão - server local
+$database = "curso_estoque"; //nome do banco de dados
+$username = "root"; // padrão - root
+$password = ""; //vazio 
+
+//Criando conexão
+$conexao = mysqli_connect($servername, $username, $password, $database);
+?>
