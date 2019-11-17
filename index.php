e19a81bc71cb64dd35d12ac7b8f07d9a7df9ff56
“joaotps22” <joaovictortps22@gmail.com>
2019-11-17 10:26:12 -0300

    Adicionando bootstrap, criando primeiro card no menu

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
-# NOTAS_ONLINE
+<html lang="pt-br">
+    <head>
+        <title>Menu_Notas</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="css/bootstrap.css">
+    </head>
+        
+    <body style="width: 500px; margin-left: 385" >
+        <center>
+    <div class="card" style="margin-top: 100px; margin-left: 100px">
+  <div class="card-header">
+    Featured
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Special title treatment</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+        </center>
+    </body>
+</html>
